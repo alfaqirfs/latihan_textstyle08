12be281c9b18ce875e47a5e9e4e8dad0e5bb197d
fauzisunarya <fauzialdzikirullah@gmail.com>
2020-09-30 09:32:34 +0700

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Latihan TextStyle'),
+        ),
+        body: Center(
+          child: Text(
+            'Ini adalah text style',
+            style: TextStyle(
+              fontFamily: 'segoeui',
+              fontStyle: FontStyle.italic,
+              fontSize: 30,
+              decoration: TextDecoration.overline,
+              decorationColor: Colors.red,
+              decorationThickness: 5,
+              decorationStyle: TextDecorationStyle.wavy,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
